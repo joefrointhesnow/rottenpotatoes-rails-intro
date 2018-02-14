5a977a3cd1e1257669cc697e2690aacf05327e27
Joe Gilbert <jgilber4@uccs.edu>
2018-02-14 17:58:22 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,10 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    #@movies = Movie.all ============================================================================9:47
+    #@movies = Movie.find(:all, :order => (params[:sort_by] ))
+    @movies = Movie.order(params[:sort_by])
+    @sort_column = params[:sort_by]
   end
 
   def new
